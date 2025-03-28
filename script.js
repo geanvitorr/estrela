8ec8b19eac6e21b0cdcb65cc82bcca16cca3f033
Gean Vitor Borges da Maia <147404323+geanvitorr@users.noreply.github.com>
2025-03-28 11:50:07 -0300

    ok

--- script.js
+++ script.js
@@ -41,3 +41,4 @@ document.addEventListener('DOMContentLoaded', function(){
  ScrollReveal().reveal('#tropicalia', { delay: 500 });
  ScrollReveal().reveal('#galeria', { delay: 500 });
  ScrollReveal().reveal('#contato', { delay: 500 });
+
